99faf135fe7c4e8d814c62bebc5bc7a9a6a6236e
juanmanuel0963 <juanmanuel0963@gmail.com>
2022-10-31 18:45:56 -0500

    commit

--- go.mod
+++ go.mod
@@ -5,6 +5,16 @@ go 1.18
 require (
 	github.com/aws/aws-lambda-go v1.34.1
 	github.com/aws/aws-sdk-go v1.44.81
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
 
 require (
---
+++ microservices/usermgmt_op1_no_persistence/usermgmt_client/usermgmt_client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/juanmanuel0963/terraform_jenkins_aws_api_gateway_microservices_lambda_golang_ec2_grpc_postgresql/v2/microservices/usermgmt_op1_no_persistence/usermgmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	//conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewUserManagementClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	var new_users = make(map[string]int32)
+	new_users["Alice"] = 43
+	new_users["Bob"] = 30
+	for name, age := range new_users {
+		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
+		if err != nil {
+			log.Fatalf("could not create user: %v", err)
+		}
+		log.Printf(`User Details:
+NAME: %s
+AGE: %d
+ID: %d`, r.GetName(), r.GetAge(), r.GetId())
+
+	}
+}
