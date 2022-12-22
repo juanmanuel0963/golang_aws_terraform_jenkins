be48c1176ada067ebc438861f99dc90a7c98c985
juanmanuel0963 <juanmanuel0963@gmail.com>
2022-12-22 13:44:59 -0500

    commit

--- microservices/usermgmt_op2_in_memory/eventbridge_server/terraform/main.tf
+++ microservices/usermgmt_op2_in_memory/eventbridge_server/terraform/main.tf
@@ -118,7 +118,8 @@ resource "aws_cloudwatch_event_target" "the_target" {
   target_id = "${var.instance_name}_${var.function_name}_target"
   arn       = "arn:aws:ssm:${var.region}::document/AWS-RunShellScript"
   //input     = "{\"commands\":[\"ls -a\"]}"
-  input     = "{\"commands\":[\"export HOME=/home/ubuntu\",\"export GOPATH=$HOME/go\",\"export GOMODCACHE=$HOME/go/pkg/mod\",\"export GOCACHE=$HOME/.cache/go-build\",\"cd /home/ubuntu/\",\"cd golang_aws_terraform_jenkins\",\"cd microservices/usermgmt_op2_in_memory/usermgmt_server\",\"go run usermgmt_server.go\"]}"
+  input     = "{\"commands\":[\"export HOME=/home/ubuntu\",\"export GOPATH=$HOME/go\",\"export GOMODCACHE=$HOME/go/pkg/mod\",\"export GOCACHE=$HOME/.cache/go-build\",\"cd /home/ubuntu/\",\"cd golang_aws_terraform_jenkins\",\"cd microservices/usermgmt_op2_in_memory/usermgmt_server\",\"usermgmt_server\"]}"
+  //input     = "{\"commands\":[\"export HOME=/home/ubuntu\",\"export GOPATH=$HOME/go\",\"export GOMODCACHE=$HOME/go/pkg/mod\",\"export GOCACHE=$HOME/.cache/go-build\",\"cd /home/ubuntu/\",\"cd golang_aws_terraform_jenkins\",\"cd microservices/usermgmt_op2_in_memory/usermgmt_server\",\"go run usermgmt_server.go\"]}"
   rule      = aws_cloudwatch_event_rule.the_rule.name
   role_arn  = aws_iam_role.the_iam_role.arn
 
