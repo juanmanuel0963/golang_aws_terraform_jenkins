5e5b3df68020ea5b544b5e2b3cb85bf3f37eadc8
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-01-10 21:06:22 -0500

    commit

--- microservices_restful_ec2/blogs/source_code/models/blog_model.go
+++ microservices_restful_ec2/blogs/source_code/models/blog_model.go
@@ -5,4 +5,11 @@ import "gorm.io/gorm"
 type Blog struct {
 	gorm.Model
 	Title string
+	Post  Post
+}
+
+type Post struct {
+	gorm.Model
+	Title string
+	Body  string
 }
--- microservices_restful_ec2/posts/source_code/migrate/migrate.go
+++ microservices_restful_ec2/posts/source_code/migrate/migrate.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/juanmanuel0963/golang_aws_terraform_jenkins/v2/microservices_restful_ec2/posts/source_code/initializers"
-	"github.com/juanmanuel0963/golang_aws_terraform_jenkins/v2/microservices_restful_ec2/posts/source_code/models"
 )
 
 func init() {
@@ -11,5 +10,5 @@ func init() {
 }
 
 func main() {
-	initializers.DB.AutoMigrate(&models.Post{})
+	//initializers.DB.AutoMigrate(&models.Post{})
 }
