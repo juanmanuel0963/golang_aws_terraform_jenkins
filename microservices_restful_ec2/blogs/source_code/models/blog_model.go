6090859338175a98bc05978a7785c195dc7b3c70
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-01-10 21:20:46 -0500

    commit

--- microservices_restful_ec2/blogs/source_code/models/blog_model.go
+++ microservices_restful_ec2/blogs/source_code/models/blog_model.go
@@ -2,14 +2,14 @@ package models
 
 import "gorm.io/gorm"
 
-type Blog struct {
-	gorm.Model
-	Title string
-	Post  Post
-}
-
 type Post struct {
 	gorm.Model
 	Title string
 	Body  string
+}
+
+type Blog struct {
+	gorm.Model
+	Title string
+	Post  Post
 }
