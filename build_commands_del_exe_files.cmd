3db2c2bdf2800c0a2a14678cc7eab7106c46f6d1
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-03-30 08:48:04 -0500

    commit

--- build_commands_del_exe_files.cmd
+++ build_commands_del_exe_files.cmd
@@ -1,10 +1,11 @@
-::--------DB migrate--------
+
+::Delete Files :: DB migrate--------
 ::ls env:
 cd D:\projects\golang_aws_terraform_jenkins\microservices_restful_ec2\_database\migrate
 del migrate
 del migrate.exe
 
-::Blogs-------------
+::Delete Files :: Blogs-------------
 ::Blogs - web server
 cd D:\projects\golang_aws_terraform_jenkins\microservices_restful_ec2\blogs\source_code
 del main
@@ -12,7 +13,7 @@ del main.exe
 del *.exe
 del *.exe~
 
-::Posts-------------
+::Delete Files :: Posts-------------
 ::Posts - web server
 cd D:\projects\golang_aws_terraform_jenkins\microservices_restful_ec2\posts\source_code
 del main
@@ -20,7 +21,7 @@ del main.exe
 del *.exe
 del *.exe~
 
-::Products-------------
+::Delete Files :: Products-------------
 ::Products - web server
 cd D:\projects\golang_aws_terraform_jenkins\microservices_restful_ec2\products\source_code
 del main
@@ -28,7 +29,7 @@ del main.exe
 del *.exe
 del *.exe~
 
-::Invoices-------------
+::Delete Files :: Invoices-------------
 ::Invoices - web server
 cd D:\projects\golang_aws_terraform_jenkins\microservices_restful_ec2\invoices\source_code
 del main
@@ -36,7 +37,49 @@ del main.exe
 del *.exe
 del *.exe~
 
-::usermgmt_op4_db_postgres-------------
+::Delete Files :: usermgmt_op1_no_persistence-------------
+
+::usermgmt_client
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op1_no_persistence\usermgmt_client
+del usermgmt_client
+del usermgmt_client.exe
+
+::usermgmt_server
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op1_no_persistence\usermgmt_server
+del usermgmt_server
+del usermgmt_server.exe
+del usermgmt_server.exe~
+
+
+::Delete Files :: usermgmt_op2_in_memory-------------
+
+::usermgmt_client
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op2_in_memory\usermgmt_client
+del usermgmt_client
+del usermgmt_client.exe
+
+::usermgmt_server
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op2_in_memory\usermgmt_server
+del usermgmt_server
+del usermgmt_server.exe
+del usermgmt_server.exe~
+
+
+::Delete Files :: usermgmt_op3_json_file-------------
+
+::usermgmt_client
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op3_json_file\usermgmt_client
+del usermgmt_client
+del usermgmt_client.exe
+
+::usermgmt_server
+cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op3_json_file\usermgmt_server
+del usermgmt_server
+del usermgmt_server.exe
+del usermgmt_server.exe~
+
+
+::Delete Files :: usermgmt_op4_db_postgres-------------
 
 ::usermgmt_client
 cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op4_db_postgres\usermgmt_client
@@ -49,7 +92,7 @@ del usermgmt_server
 del usermgmt_server.exe
 del usermgmt_server.exe~
 
-::usermgmt_op5_rest_to_grpc-------------
+::Delete Files :: usermgmt_op5_rest_to_grpc-------------
 
 ::usermgmt_client
 cd D:\projects\golang_aws_terraform_jenkins\microservices_grpc_ec2\usermgmt_op5_rest_to_grpc\usermgmt_client
