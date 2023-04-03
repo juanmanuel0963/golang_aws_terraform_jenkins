41985bf53615a8af5885ed2afcf66b5b1cccf88e
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-04-03 17:51:59 -0500

    commit

--- step_5_deploy_tools.cmd
+++ step_5_deploy_tools.cmd
@@ -8,5 +8,5 @@ terraform workspace list
 terraform init
 terraform validate
 terraform plan -var db_password=Suta100* -out "output_plan_tools.tfplan"
-::terraform apply -auto-approve "output_plan_tools.tfplan"
+terraform apply -auto-approve "output_plan_tools.tfplan"
 ::--terraform destroy -auto-approve
