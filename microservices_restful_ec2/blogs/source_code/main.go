332fe56dacd615faed88d17f40d5c368396dab6a
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-04-10 20:35:08 -0500

    commit

--- microservices_restful_ec2/blogs/source_code/main.go
+++ microservices_restful_ec2/blogs/source_code/main.go
@@ -24,10 +24,10 @@ func main() {
 	//err := r.Run(":" + os.Getenv("PORT"))
 	//r.Run() // listen and serve on 0.0.0.0:env(PORT)
 
-	//Local w/TLS
+	//Local w/ TLS
 	//err := r.RunTLS(":"+os.Getenv("PORT"), "cert.pem", "key.pem")
 
-	//Local wout/TLS
+	//Local w/out TLS
 	//err := r.Run(":" + os.Getenv("PORT"))
 
 	//Server
