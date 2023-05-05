7f11d9b93a7a5225931219eab0a46c1452b1c12d
juanmanuel0963 <juanmanuel0963@gmail.com>
2023-05-05 14:35:38 -0500

    commit

--- step_5_deploy_dev.cmd
+++ step_5_deploy_dev.cmd
@@ -9,4 +9,4 @@ terraform init
 terraform validate
 terraform plan -var db_password="Suta100*" -out "output_plan_dev.tfplan"
 terraform apply -auto-approve "output_plan_dev.tfplan"
-terraform destroy -auto-approve
+::terraform destroy -auto-approve
